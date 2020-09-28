72b63edd782ebf1d7d0e2c31b0e310207dd60450
Nguyen Marc <nguyen_marc@live.fr>
2020-09-28 15:51:16 +0200

    [main] Handles port env var

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,13 @@ import { NestFactory } from '@nestjs/core';
 import { BookModule } from './book.module';
 
 async function bootstrap() {
+  const port = process.env.PORT;
   const app = await NestFactory.create(BookModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  if (port) {
+    await app.listen(port);
+  } else {
+    await app.listen(3000);
+  }
 }
 bootstrap();
